129c6f92a2e4a42de65be3c8de9c0b16939128b6
PHET45 <phnjk2000@gmail.com>
2025-11-09 20:45:18 +0700

    feat: ex.1

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,13 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        // ประกาศตัวแปร
+        String name = "Phet";
+        int age = 23;
+        String hobby = "coding";
+
+        // แสดงผลด้วยการเชื่อมข้อความด้วย +
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My hobby is " + hobby + ".");
+    }
+}
